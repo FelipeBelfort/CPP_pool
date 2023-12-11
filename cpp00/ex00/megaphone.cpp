3bdea5f1867e9b0ae293533e535672cab1746127
fbelfort42 <fbelfort@student.42lyon.fr>
2023-12-11 17:18:48 +0100

    ex00 refacto and ex01 folder creation

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -2,21 +2,19 @@
 
 int	main(int argc, char **argv)
 {
-	int		    i;
-	int		    j;
-	int		    max;
-    std::string str;
+	int		    			word;
+	std::string 			str;
+	std::string::iterator	iter;
 
-	j = 0;
+	word = 0;
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	while (argc > 1 && argv && argv[++j])
+	while (argv && argv[++word])
 	{
-		i = -1;
-        str = argv[j];
-        max = str.size();
-		while (++i < max)
-			std::cout << (char)std::toupper(str.at(i));
+        str = argv[word];
+		iter = str.begin();
+		while (iter != str.end())
+			std::cout << (char)std::toupper(*iter++);
 	}
 	std::cout << std::endl;
 	return (0);
