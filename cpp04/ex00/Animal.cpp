59ddd5d848f679e4572d8d9f80ec0c7dfb5a8fe7
fbelfort42 <fbelfort@student.42lyon.fr>
2024-01-09 16:17:52 +0100

    cpp04 creation

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,19 @@
+#include "Animal.hpp"
+
+Animal::Animal(void)
+{
+}
+
+Animal::~Animal(void)
+{
+}
+
+Animal::Animal(const Animal &copy)
+{
+
+}
+
+Animal	&Animal::operator=(const Animal &copy)
+{
+
+}
