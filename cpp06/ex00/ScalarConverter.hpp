0b5d12670edafabe852ac17cca91e487988339a8
fbelfort42 <fbelfort@student.42lyon.fr>
2024-02-07 20:31:26 +0100

    ex00 done

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,32 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+# include <string>
+# include <climits>
+# include <cmath>
+# include <cctype>
+
+# define T_VOID 0
+# define T_CHAR 1
+# define T_INT 2
+# define T_FLOAT 3
+# define T_DOUBLE 4
+
+typedef	void	(*func)(char *);
+
+class ScalarConverter
+{
+private:
+	ScalarConverter(void);
+	~ScalarConverter(void);
+
+	static double	_value;
+public:
+	static void	convert(char *input);
+};
+
+
+
+
+#endif
