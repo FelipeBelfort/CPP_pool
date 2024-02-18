f8f9703e6ca1040b8465c0d0789e552f6e39edab
Felipe Belfort <felipebelfort@gmail.com>
2024-02-18 03:46:12 +0100

    ex00 done-to test

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,158 +3,186 @@
 
 BitcoinExchange::BitcoinExchange(char *data, char *input)
 {
-	if (!data || !input)
-		return ;
+	if (!data)
+		throw NoDataBaseException();
+	if (!input)
+		throw NoInputException();
 	this->_db_file.open(data);
 	if (!this->_db_file.is_open())
-		return ;
-	// (void)input;
+		throw NoDataBaseException();
 	this->_input_file.open(input);
 	if (!this->_input_file.is_open())
 	{
 		this->_db_file.close();
-		return ;
+		throw NoInputException();
 	}
 	this->fillData();
 	this->readInput();
-	// DEBUG
-	// this->fillMap(DATA);
-	// this->fillMap(INPUT);
-	this->_db_file.close();
-	this->_input_file.close();
-
 }
 
 BitcoinExchange::~BitcoinExchange(void)
 {
+	this->_db_file.close();
+	this->_input_file.close();
 }
 
-void	BitcoinExchange::fillData()
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy)
+{
+	*this = copy;
+}
+
+BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &copy)
+{
+	if (this != &copy)
+		this->_data = copy._data;
+	return *this;
+}
+
+void	BitcoinExchange::fillData(void)
 {
 	std::string	tmp;
 
-		// std::map<std::string, double>::iterator	it = this->_data.begin();
 	while (std::getline(this->_db_file, tmp))
 		this->parseDataLine(tmp);
 }
 
-void	BitcoinExchange::parseDataLine(std::string str)
+void	BitcoinExchange::parseDataLine(std::string &str)
 {
 	std::string												key;
 	std::pair<std::map<std::string, float>::iterator, bool>	ret;
 	float													nb;
 
-	// std::cout << "size => " << str.size() << " str[10] => " << str[10] << " not of => " << str.find_first_not_of("-0123456789,.") << " first of => " << str.find_first_of("0123456789", 11) << std::endl;
+	if (str == "date,exchange_rate")
+		return ;
 	if (str.size() < 12 || str[10] != ',' || str.find_first_not_of("-0123456789,.") != std::string::npos || str.find_first_of("0123456789", 11) != 11)
-		return ;
-	// std::cout << "data => " << str.substr(0, 10) << std::endl;
-	key = this->parseDate(str.substr(0, 10));
-	if (key.empty())
-		return ;
+		throw CorrompedDataBaseException();
+	key = str.substr(0, 10);
+	try
+	{
+		this->parseDate(key);
+	}
+	catch(const std::exception& e)
+	{
+		throw CorrompedDataBaseException();
+	}
 	nb = std::atof(str.c_str() + 11);
 	if (nb < 0)
-		return ;
+		throw CorrompedDataBaseException();
 	ret = this->_data.insert(std::pair<std::string, double>(key, nb));
-	// std::cout << (*ret.first).first << " => " << this->_data[key] << "#" << nb << std::endl;
 	if (ret.second == false)
-		return ;
+		throw DuplicatedDataBaseException();
 }
 
-std::string	BitcoinExchange::parseDate(std::string str)
+void	BitcoinExchange::parseDate(std::string &str)
 {
 	int	year;
 	int	month;
 	int	day;
 
-	// if (str.size() != 10)
-	// 	return NULL;
-	if (str[4] != '-' || str[7] != '-')
-		return NULL;
-	// str[4] = '#';
-	// str[7] = '#';
-	// std::cout << "a => " << str.find_first_not_of("0123456789-") << " b => " << str.find_first_of("-") << " c => " << str.find_first_of("-", 5) << " d => " << str.find_first_of("-", 8) << std::endl;
 	if (str.find_first_not_of("0123456789-") != std::string::npos || str.find_first_of("-") != 4
 		|| str.find_first_of("-", 5) != 7 || str.find_first_of("-", 8) != std::string::npos)
-		return NULL;
+		throw InputErrorException();
 	year = atoi(str.c_str());
 	month = atoi(str.c_str() + 5);
 	day = atoi(str.c_str() + 8);
 
-	// if (year < 0)
-	// 	return NULL;
-	if (month < 1 || month > 12)
-		return NULL;
-	if (day < 1)
-		return NULL;
-	// DEBUG
-	if (month == 2)
-	{
-		if (day > 29 && ((year % 400 == 0) || ( (year % 4 == 0) && (year % 100 != 0))))
-			return NULL;
-		else if (day > 28)
-			return NULL;
-	}
-	if ((month < 8 && month % 2) || (month > 7 && month % 2 == 0))
-		if (day > 31)
-			return NULL;
-	if ((month < 8 && month % 2 == 0) || (month > 7 && month % 2))
-		if (day > 30)
-			return NULL;
-	// str[4] = '#';
-	// str[7] = '#';
-	return str;
+	if (month < 1 || month > 12 || day < 1)
+		throw InputErrorException();
+	if (month == 2 && ((day > 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) || day > 28))
+		throw InputErrorException();
+	if (day > 31 && ((month < 8 && month % 2) || (month > 7 && month % 2 == 0)))
+		throw InputErrorException();
+	if (day > 30 && ((month < 8 && month % 2 == 0) || (month > 7 && month % 2)))
+		throw InputErrorException();
 }
 
 void	BitcoinExchange::readInput(void)
 {
 	std::string	tmp;
 
-		// std::map<std::string, double>::iterator	it = this->_data.begin();
-	std::getline(this->_input_file, tmp);
-	// std::getline(this->_input_file, tmp);
 	while (std::getline(this->_input_file, tmp))
-		this->parseInputLine(tmp);
+	{
+		try
+		{
+			this->parseInputLine(tmp);
+		}
+		catch(const BitcoinExchange::InputErrorException& e)
+		{
+			std::cout << e.what() << tmp << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+
+	}
 }
 
-void	BitcoinExchange::parseInputLine(std::string str)
+void	BitcoinExchange::parseInputLine(std::string &str)
 {
 	std::string								key;
 	float									nb;
 	std::map<std::string, float>::iterator	it;
-	// std::pair<std::map<std::string, float>::iterator, bool>	ret;
 
-	// std::cout << "size => " << str.size() << " str[10] => " << str.find(" | ") << " not of => " << str.find_first_not_of("-0123456789|. ") << " first of => " << str.find_first_of("0123456789", 13) << std::endl;
-	// return ;
+	if (str == "date | value")
+		return ;
 	if (str.size() < 14 || str.find(" | ") != 10 || str.find_first_not_of("-0123456789| .") != std::string::npos || str.find_first_of("0123456789", 13) > 14)
-		return ;
-	// DEBUG
-	// std::cout << "data => " << str.substr(0, 10) << std::endl;
-	key = this->parseDate(str.substr(0, 10));
-	if (key.empty())
-		return ;
+		throw InputErrorException();
+	key = str.substr(0, 10);
+	this->parseDate(key);
 	nb = std::atof(str.c_str() + 12);
 	if (nb < 0)
-		return ;
+		throw NegativeNumberException();
 	if (nb > 1000.0)
-		return ;
+		throw TooLargeNumberException();
 	it = this->_data.find(key);
-	if (it != this->_data.end())
-		std::cout << (*it).first << " => " << nb * (*it).second << std::endl;
+	if ((*it).first == key)
+		std::cout << key << " => " << nb << " = " << nb * (*it).second << std::endl;
 	else
 	{
 		it = this->_data.lower_bound(key);
-		if (it != this->_data.end())
-			std::cout << (*it).first << " => " << nb * (*it).second << std::endl;
-		else
-			std::cout << "bla => " << std::endl;
+		if (it == this->_data.end())
+			throw DateNotFoundException();
+		std::cout << (*it).first << " => " << nb << " = " << nb * (*it).second << std::endl;
 	}
-	// for (std::map<std::string, float>::iterator it = this->_data.begin(); it != this->_data.end(); it++)
-	// {
-	// 	// if ((*it).first == key)
-	// }
+}
 
-	// ret = this->_data.insert(std::pair<std::string, double>(key, nb));
-	// std::cout << (*ret.first).first << " => " << this->_data[key] << "#" << nb << std::endl;
-	// if (ret.second == false)
-	// 	return ;
+const char	*BitcoinExchange::NoDataBaseException::what() const throw()
+{
+	return "Error: could not open csv file.";
+}
+
+const char	*BitcoinExchange::CorrompedDataBaseException::what() const throw()
+{
+	return "Error: the data in the csv file seems not compatible with the requeriments.";
+}
+
+const char	*BitcoinExchange::NoInputException::what() const throw()
+{
+	return "Error: could not open file.";
+}
+
+const char	*BitcoinExchange::InputErrorException::what() const throw()
+{
+	return "Error: bad input => ";
+}
+
+const char	*BitcoinExchange::DuplicatedDataBaseException::what() const throw()
+{
+	return "Error: there is a duplicated data in the csv file";
+}
+
+const char	*BitcoinExchange::NegativeNumberException::what() const throw()
+{
+	return "Error: not a positive number.";
+}
+
+const char	*BitcoinExchange::TooLargeNumberException::what() const throw()
+{
+	return "Error: too large a number.";
+}
+
+const char	*BitcoinExchange::DateNotFoundException::what() const throw()
+{
+	return "Error: date not found in the database (too old)";
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,59 +6,66 @@
 # include <iostream>
 # include <fstream>
 
-# define DEBUG std::cout << "pas moi" << std::endl;
-// # define DATA true
-// # define INPUT false
-
 class BitcoinExchange
 {
 private:
 	std::map<std::string, float>	_data;
-	// std::map<std::string, double>	_input;
 	std::fstream					_db_file;
 	std::fstream					_input_file;
 
 	BitcoinExchange(void);
-public:
-	BitcoinExchange(char *data, char *input);
-	~BitcoinExchange(void);
 	BitcoinExchange(const BitcoinExchange &copy);
 	BitcoinExchange	&operator=(const BitcoinExchange &copy);
 
+	void	fillData(void);
+	void	parseDate(std::string &str);
+	void	parseDataLine(std::string &str);
+	void	parseInputLine(std::string &str);
+	void	readInput(void);
+public:
+	BitcoinExchange(char *data, char *input);
+	~BitcoinExchange(void);
 
-	void		fillData(void);
-	std::string	parseDate(std::string str);
-	void		parseDataLine(std::string str);
-	void		parseInputLine(std::string str);
-	void		readInput(void);
 
 	class NoDataBaseException : public std::exception
 	{
+		public:
+		const char	*what() const throw();
+	};
+	class CorrompedDataBaseException : public std::exception
+	{
+		public:
 		const char	*what() const throw();
 	};
 	class NoInputException : public std::exception
 	{
+		public:
 		const char	*what() const throw();
 	};
 	class InputErrorException : public std::exception
 	{
+		public:
 		const char	*what() const throw();
 	};
 	class DuplicatedDataBaseException : public std::exception
 	{
+		public:
 		const char	*what() const throw();
 	};
-	// class NoDataBaseException : public std::exception
-	// {
-	// 	const char	*what() const throw();
-	// };
-	// class NoDataBaseException : public std::exception
-	// {
-	// 	const char	*what() const throw();
-	// };
-	// class NoDataBaseException : public std::exception
-	// {
-	// 	const char	*what() const throw();
+	class NegativeNumberException : public std::exception
+	{
+		public:
+		const char	*what() const throw();
+	};
+	class TooLargeNumberException : public std::exception
+	{
+		public:
+		const char	*what() const throw();
+	};
+	class DateNotFoundException : public std::exception
+	{
+		public:
+		const char	*what() const throw();
 	};
 };
 
